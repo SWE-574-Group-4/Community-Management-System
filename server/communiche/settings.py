ac0682b39002bad98a1a12657b19d9a0712b0f23
Onur Altınkurt <onuraltinkurt@nimportequoi.local>
2024-10-19 14:39:13 +0300

    DB Connection Update

--- server/communiche/settings.py
+++ server/communiche/settings.py
@@ -90,8 +90,8 @@ DATABASES = {
         'NAME': os.getenv('DB_NAME'),
         'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
 
