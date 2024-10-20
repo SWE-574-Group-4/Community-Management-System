f660a9cd50bf7bc250e93d9c8193c48ecb91f78b
Onur Altınkurt <onuraltinkurt@nimportequoi.local>
2024-10-20 19:23:03 +0300

    Logo color is changed for demo video

--- client/src/components/shared/LogoAsText.tsx
+++ client/src/components/shared/LogoAsText.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function LogoAsText() {
     return (
-        <span className="dark:text-sky-50 text-black font-thin text-3xl hidden lg:block">
+        <span className="dark:text-sky-50 text-black font-thin text-3xl hidden lg:block" style="color: #32cd32;">
             COMMU<span className="font-extrabold">NICHE</span>
         </span>
     )
