295ba0469d47e4ebff5324942b9322297c30be16
Onur Altınkurt <onuraltinkurt@nimportequoi.local>
2024-10-20 19:35:27 +0300

    Reverted color change

--- client/src/components/shared/LogoAsText.tsx
+++ client/src/components/shared/LogoAsText.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function LogoAsText() {
     return (
-        <span className="dark:text-sky-50 text-black font-thin text-3xl hidden lg:block" style="color: #32cd32;">
+        <span className="dark:text-sky-50 text-black font-thin text-3xl hidden lg:block">
             COMMU<span className="font-extrabold">NICHE</span>
         </span>
     )
