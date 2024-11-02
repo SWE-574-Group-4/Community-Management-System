af4fd3c70a177b35573e1736c034019490e94dd4
Irem Kurt <89161696+irem-kurt@users.noreply.github.com>
2024-11-02 23:35:16 +0300

    remove page on navbar

--- client/src/configs/routes.config/routes.config.ts
+++ client/src/configs/routes.config/routes.config.ts
@@ -50,16 +50,16 @@ export const protectedRoutes = [
             headerContainer: true,
         },
     },
-    {
-        key: 'post',
-        path: '/post/:id',
-        component: lazy(() => import('@/views/post/PostView')),
-        authority: [],
-        meta: {
-            header: i18n.t('nav.post'),
-            headerContainer: true,
-        },
-    },
+    // {
+    //     key: 'post',
+    //     path: '/post/:id',
+    //     component: lazy(() => import('@/views/post/PostView')),
+    //     authority: [],
+    //     meta: {
+    //         header: i18n.t('nav.post'),
+    //         headerContainer: true,
+    //     },
+    // },
     {
         key: 'post',
         path: '/profile/:id',
