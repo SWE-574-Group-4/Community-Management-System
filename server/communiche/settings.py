499a9823ac9d24ff2ca559decaec92102f633b85
elifdonmez <donmez.elif.c@gmail.com>
2024-11-03 14:44:10 +0300

    [52]: Settings.py update: database config is reverted

--- server/communiche/settings.py
+++ server/communiche/settings.py
@@ -87,11 +87,11 @@ load_dotenv()
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'communiche_db',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': 3306,
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
 
