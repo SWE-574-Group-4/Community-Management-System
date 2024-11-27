d080dfaabcd106f40b2ab559ba4421498d4fb331
Mehmet Eyüpoğlu <mehmeteyupoglu2@gmail.com>
2024-11-27 23:27:32 +0300

    feat: update image source handling in RenderField component

--- client/src/views/post/components/RenderField.tsx
+++ client/src/views/post/components/RenderField.tsx
@@ -10,9 +10,7 @@ export default function RenderField({ field }: { field: _Field }) {
             {field.field_type === 'textarea' && <p>{field.field_value}</p>}
             {field.field_type === 'image' && field.field_value !== '' && (
                 <img
-                    src={
-                        'https://static.vecteezy.com/system/resources/previews/009/944/861/non_2x/any-question-label-template-design-vector.jpg'
-                    }
+                    src={field.field_value}
                     alt="Image"
                     style={{ maxWidth: '400px' }}
                     className="rounded-lg"
