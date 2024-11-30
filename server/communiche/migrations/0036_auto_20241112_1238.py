a9b3a2de7299118f5037c2b0affc46bd798dfa2d
Rashid <rashidgaitov@gmail.com>
2024-11-30 16:33:08 +0300

    Remove Tag model creation from migration and add merge migration for future changes

--- server/communiche/migrations/0036_auto_20241112_1238.py
+++ server/communiche/migrations/0036_auto_20241112_1238.py
@@ -10,13 +10,6 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.CreateModel(
-            name='Tag',
-            fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('name', models.CharField(max_length=50, unique=True)),
-            ],
-        ),
         migrations.AddField(
             model_name='posts',
             name='tags',
