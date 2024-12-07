500894f7725ab353e3de41288e9a9daa4a184a7b
Burak Cosar <csr.burak@icloud.com>
2024-12-07 12:09:46 +0300

    Add enumerated field type to the model

--- server/communiche/migrations/0013_auto_20240406_1239.py
+++ server/communiche/migrations/0013_auto_20240406_1239.py
@@ -7,7 +7,7 @@ def create_fields(apps, schema_editor):
     DataFields.objects.create(data_type='text', label='Text')
     DataFields.objects.create(data_type='date', label='Date')
     DataFields.objects.create(data_type='geo', label='Geolocation')
-    DataFields.objects.create(data_type='enum', label='Enumerated', preferred_keyword='')
+    DataFields.objects.create(data_type='enum', label='Enumerated', preferred_keyword='') # enumerated field
     DataFields.objects.create(data_type='number', label='Number')
     DataFields.objects.create(data_type='image', label='Image')
     DataFields.objects.create(data_type='video', label='Video')
